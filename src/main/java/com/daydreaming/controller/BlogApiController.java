b478a45a02f4714d19559c38f9873cabd525f641
jayoon-oh <dhwkdbs3@gmail.com>
2025-02-28 17:23:40 +0900

    추가, 수정 메서드 & 테스트 추가

--- src/main/java/com/daydreaming/controller/BlogApiController.java
+++ src/main/java/com/daydreaming/controller/BlogApiController.java
@@ -2,14 +2,15 @@ package com.daydreaming.controller;
 
 import com.daydreaming.domain.Article;
 import com.daydreaming.dto.AddArticleRequest;
-import com.daydreaming.repository.BlogRepository;
+import com.daydreaming.dto.ArticleResponse;
+import com.daydreaming.dto.UpdateArticleRequest;
 import com.daydreaming.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // JSON 형식 변환
@@ -25,5 +26,45 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    // 전체 글 조회 후 반환
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable Long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    // 글 삭제 메서드
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    // 글 수정
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable Long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updateArticle = blogService.update(id, request);
+
+        return  ResponseEntity.ok()
+                .body(updateArticle);
     }
 }
--- src/main/java/com/daydreaming/service/BlogService.java
+++ src/main/java/com/daydreaming/service/BlogService.java
@@ -2,9 +2,14 @@ package com.daydreaming.service;
 
 import com.daydreaming.domain.Article;
 import com.daydreaming.dto.AddArticleRequest;
+import com.daydreaming.dto.UpdateArticleRequest;
 import com.daydreaming.repository.BlogRepository;
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor // final 붙거나 @NotNull 이 붙은 빌드 생성자 추가
 @Service
@@ -15,5 +20,30 @@ public class BlogService {
     // 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() { // Article 에 저장되어 있는 모든 데이터 조회
+        return blogRepository.findAll();
+    }
+
+    public Article findById(Long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Article not found" + id));
+    }
+
+    // 글 삭제 메서드
+    public void delete(Long id) {
+        blogRepository.deleteById(id);
+    }
+
+    // 글 수정 메서드
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Article not found" + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
